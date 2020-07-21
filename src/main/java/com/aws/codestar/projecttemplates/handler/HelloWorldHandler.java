be23b05d30700e428cdd50b554825f00d874c648
Gopal <Gopal@LAPTOP-V3MG2SM2.softeon.com>
2020-07-21 18:33:59 -0400

    Version OkOKOK3

--- src/main/java/com/aws/codestar/projecttemplates/handler/HelloWorldHandler.java
+++ src/main/java/com/aws/codestar/projecttemplates/handler/HelloWorldHandler.java
@@ -10,7 +10,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * Handler for requests to Lambda function. OKOK
+ * Handler for requests to Lambda function. OKOKOK3
  */
 public class HelloWorldHandler implements RequestHandler<Object, Object> {
 
